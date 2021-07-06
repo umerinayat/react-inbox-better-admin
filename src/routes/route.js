efa169d7e429891ea961fd81a7411b93ed6b0808
Umer Inayat <umer@pkteam.com>
2021-07-06 15:25:56 +0500

    added login route redirection to dashboard if user login

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+
 
 const AppRoute = ({
   component: Component,
@@ -10,20 +11,31 @@ const AppRoute = ({
   ...rest
 }) => {
 
+  const location = useLocation();
   const isLoggedIn = useSelector(state => state.authReducer.isLoggedIn);
 
   return (
     <Route
       {...rest}
       render={(props) => {
+        
+        
+        if(location.pathname === '/login' && isLoggedIn)
+        {
+          return (<Redirect
+            to={{ pathname: '/dashboard', state: { from: props.location } }}
+          />);
+        }
+
         if (isAuthProtected && !isLoggedIn) {
+
           return (
             <Redirect
               to={{ pathname: '/login', state: { from: props.location } }}
             />
           );
         }
-
+        
         return (
           <Layout>
             <Component {...props} />
